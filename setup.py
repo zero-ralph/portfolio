464510a7f23b2408515cb7f0841e271b4eecff80
zero-ralph <ralph.subrio@gmail.com>
2026-04-30 22:52:44 +0800

    Add make file for linting/pre-commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'ruff',
+    'detect-secrets'
+]
+
+setup(
+    name="MTSP",
+    version="1.0.0",
+    author="Ralph Subrio",
+    author_email="ralph.subrio@gmail.com",
+    description="Multi Tenant SaaS Portfolio",
+    license="BSD",
+    packages=find_packages('.'),
+    install_requires=requires
+)
